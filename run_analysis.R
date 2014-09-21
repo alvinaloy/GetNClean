3c15b4b564c7f02bdb9d336c4d1a7fd75ca3e97e
Alvin Goh <alvinaloy@hotmail.com>
2014-09-21 15:24:20 +0800

    Committed

--- run_analysis.R
+++ run_analysis.R
@@ -3,6 +3,7 @@
 fileUrl <- "http://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl, destfile = "Dataset.zip")
 unzip("Dataset.zip")
+
 test_x <- read.table("UCI HAR Dataset/test/X_test.txt")
 test_y <- read.table("UCI HAR Dataset/test/y_test.txt")
 test_sub <- read.table("UCI HAR Dataset/test/subject_test.txt")
@@ -15,8 +16,8 @@ activities <- read.table("UCI HAR Dataset/activity_labels.txt", colClasses = "ch
 test_y$V1 <- factor(test_y$V1, levels = activities$V1, labels = activities$V2)
 train_y$V1 <- factor(train_y$V1, levels = activities$V1,labels = activities$V2)
 
-# 4. Appropriately labels the data set with descriptive activity names
-features <- read.table("UCI HAR Dataset/features.txt", colClasses="character")
+# 4. Appropriately label the data set with descriptive activity names
+features <- read.table("UCI HAR Dataset/features.txt", colClasses = "character")
 colnames(test_x) <- features$V2
 colnames(train_x) <- features$V2
 colnames(test_y) <- "Activity"
@@ -33,7 +34,7 @@ allData <- rbind(testData,trainData)
 extract_col <- grepl("Activity|Subject|mean|std", names(allData))
 extAllData <- allData[ , extract_col]
 
-# 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# 5. Create a second, independent tidy data set with the average of each variable for each activity and each subject.
 extract_col2 <- grepl("Activity|Subject|mean", names(extAllData))
 
 tidyData <- extAllData[ , extract_col2]
